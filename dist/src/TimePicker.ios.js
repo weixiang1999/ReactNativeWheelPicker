5e079397fa615bd3c4a7fdce7d18955389884e4b
weixiang <920666487@qq.com>
2020-07-06 13:31:18 +0800

    chore: 编译

--- dist/src/TimePicker.ios.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import DatePicker from './DatePicker.ios';
-const TimePicker = props => {
-    return (<DatePicker mode={'time'} {...props} onDateSelected={props.onTimeSelected}/>);
-};
-export default TimePicker;
